164205e7c62b1f604db8d2206eb6a7281c7f6a70
genshinya <876737761@qq.com>
2024-05-22 09:03:04 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/CSNotebook/",
 
   lang: "zh-CN",
   title: "Notebook",
